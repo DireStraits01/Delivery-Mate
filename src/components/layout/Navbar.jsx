f55217c27760486bbb7f228ba504f1ad5defa098
DireStraits01 <arsamak88@gmail.com>
2023-08-02 14:13:52 +0300

    input forms

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
         <ul>
           <li>
             <NavLink
-              to="/createAd"
+              to="/CreateAd"
               className={({ isActive }) => (isActive ? style.activeLink : '')}
             >
               добавить объявление
--- src/components/pages/ad/CreateAd.jsx
+++ src/components/pages/ad/CreateAd.jsx
@@ -7,24 +7,45 @@ function CreateAd() {
       <div className="role__wrapper">
         <div className="container_content">
           <img className="role__image" src={imgChooseRole} alt="" />
-          <div className="role__ad_form">
-            <div className="role__radio">
-              <label>
-                Я клиент
-                <input type="radio" value="sender" name="chooseRole" />
-              </label>
 
-              <label>
-                Я курьер
-                <input type="radio" value="courier" name="chooseRole" />
-              </label>
-            </div>
-            <div className="role__direction">
-              <label htmlFor="startCity">
-                город отправления: <input type="text" value="" id="startCity" />
-              </label>
-            </div>
-          </div>
+          <form action="" className="role__ad_form">
+            <fieldset>
+              <legend>Параметры заявки</legend>
+              <div className="role__radio">
+                <label>
+                  <input type="radio" value="sender" name="chooseRole" />Я
+                  отправитель
+                </label>
+
+                <label>
+                  <input type="radio" value="courier" name="chooseRole" />Я
+                  курьер
+                </label>
+              </div>
+              <div className="textInput">
+                <div className="col_label">
+                  <label htmlFor="cityDeparture">Город отправления:</label>
+                  <label htmlFor="cityDelivery">Город прибытия:</label>
+                  <label htmlFor="date">Дата:</label>
+                </div>
+
+                <div className="col_input">
+                  <input type="text" id="cityDeparture" />
+                  <input type="text" id="cityDelivery" />
+                  <input type="date" id="date" />
+                </div>
+              </div>
+
+              {/* <label htmlFor="startCity" className="textareaForm">
+                дополнительная информация:&nbsp;
+                <textarea name="" id="" cols="10" rows="30"></textarea>
+              </label> */}
+              <div className="form_btns">
+                <button type="submit">Отправить</button>
+                <button>Назад</button>
+              </div>
+            </fieldset>
+          </form>
         </div>
       </div>
     </>
